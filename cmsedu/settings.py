eb1e71d8044a17f3c85dfb413056d4c35a189915
unknown <etemkeskin@yahoo.com>
2022-04-25 10:00:40 +0300

    created post model

--- cmsedu/settings.py
+++ cmsedu/settings.py
@@ -79,10 +79,14 @@ WSGI_APPLICATION = 'cmsedu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { #LOCAL
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cmsedu', 
+        'USER': 'root', 
+        'PASSWORD': '123456', 
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+from venv import create
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+
+
