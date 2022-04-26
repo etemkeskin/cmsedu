8deb644ea55f60bf92a44631d5120c4b0165590e
unknown <etemkeskin@yahoo.com>
2022-04-26 22:39:36 +0300

    create post

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,11 @@ app_name = 'main'
 urlpatterns = [
     path('home/' , home, name='home'),
     path('about/' , about, name='about'),
+
+    # path('blogs/' , blog_list, name='blog_list'),
+
+    path('blogs/create/' , blog_create, name='blog_create')
+    # path('blogs/<int:id>/' , get_blog, name='get_blog')
+    # path('blogs/<int:id>/update/' , update_blog, name='update_blog')
+    # path('blogs/<int:id>/delete/' , delete_blog, name='delete_blog')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from main.models import Post
 
 
 def home(request):
@@ -8,3 +9,11 @@ def home(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def blog_create(request):
+
+    if request.method == "POST":
+        print(request)
+        Post.objects.create(user_id = 1, title = request.POST["title"], content = request.POST["content"])
+
+    return render(request, 'blog/create.html')
